b2c85a5f77d432c55317b1a13dd90e89f6523276
LeinadSaia <81182977+LeinadSaia@users.noreply.github.com>
2021-04-13 18:43:48 -0300

    Update index.js

--- index.js
+++ index.js
@@ -300,11 +300,11 @@ async function starts() {
 			}
 
 			const botNumber = client.user.jid
-			const ownerNumber = ["554792091566@s.whatsapp.net"] // replace this with your number
-			const mod = [ownerNumber,"554792091566@s.whatsapp.net"]//ubah nomor lo
-			const adminbotnumber = ["554792091566@s.whatsapp.net"]//ubah nomor lo
-			const frendsowner = ["554792091566@s.whatsapp.net"]//ubah nomor lo
-			const premium = ["554792091566@s.whatsapp.net","558296156440@s.whatsapp.net"] 
+			const ownerNumber = ["5522996215481@s.whatsapp.net","559884527526@s.whatsapp.net","559887386952@s.whatsapp.net","559884814675@s.whatsapp.net"] // replace this with your number
+			const mod = [ownerNumber,"5522996215481@s.whatsapp.net","559884527526@s.whatsapp.net","559887386952@s.whatsapp.net","559884814675@s.whatsapp.net"]//ubah nomor lo
+			const adminbotnumber = ["5522996215481@s.whatsapp.net","559884527526@s.whatsapp.net","559887386952@s.whatsapp.net","559884814675@s.whatsapp.net"]//ubah nomor lo
+			const frendsowner = ["5522996215481@s.whatsapp.net","559884527526@s.whatsapp.net","559887386952@s.whatsapp.net","559884814675@s.whatsapp.net"]//ubah nomor lo
+			const premium = ["5522996215481@s.whatsapp.net","559884527526@s.whatsapp.net","559887386952@s.whatsapp.net","559884814675@s.whatsapp.net"] 
 			const isGroup = from.endsWith('@g.us')
 			const sender = isGroup ? mek.participant : mek.key.remoteJid
 			const groupMetadata = isGroup ? await client.groupMetadata(from) : ''
